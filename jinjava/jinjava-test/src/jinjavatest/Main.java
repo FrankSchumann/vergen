2dc167834571ed7eb909fa9579b6cb84d5ac1e82
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-19 10:27:12 +0200

    fix ticket: Write result for template_1 to file

--- jinjava/jinjava-test/src/jinjavatest/Main.java
+++ jinjava/jinjava-test/src/jinjavatest/Main.java
@@ -4,6 +4,7 @@ import com.hubspot.jinjava.Jinjava;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.io.IOException;
 import java.util.*;
 
@@ -41,6 +42,14 @@ public class Main {
 			
 			String renderedTemplate_1 = jinjava2.render(template_1, context_1);
 			System.out.println(renderedTemplate_1);
+			
+			Path pathOutputFile = Paths.get("/home/frank/output.txt");
+			
+			if(false == Files.exists(pathOutputFile)){
+			  //  Files.createFile(pathOutputFile);
+			}
+			
+			Files.write(pathOutputFile, renderedTemplate_1.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
 		
 		
 		}
