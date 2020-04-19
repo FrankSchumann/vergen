d4cf358c4e32a08a47e2d4ca08bc173b9c038388
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-19 09:39:45 +0200

    fix ticket: Read String from file

--- jinjava/jinjava-test/src/jinjavatest/Main.java
+++ jinjava/jinjava-test/src/jinjavatest/Main.java
@@ -1,12 +1,33 @@
 package jinjavatest;
 
 import com.hubspot.jinjava.Jinjava;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.IOException;
 import java.util.*;
+
 
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		try
+		{
+			Path path = Paths.get("/home/frank/test.txt");
+			byte[] fileContentBytes = Files.readAllBytes(path);
+			String content = new String(fileContentBytes);
+			
+			System.out.println(content);
+			
+//			String content = new String(Files.readAllBytes(Paths.get("test.txt")));
+		}
+		catch (IOException e) 
+	    {
+	        e.printStackTrace();
+	    }
+		
 		
 		Jinjava jinjava2 = new Jinjava();
 		
