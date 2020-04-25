0631e137b903b6fc0621eb4706d8c6323305243c
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-25 11:25:48 +0200

    add minor, bugfix and build

--- parser/src/parser/Main.java
+++ parser/src/parser/Main.java
@@ -7,10 +7,19 @@ public class Main
        {
              System.out.println("Hello World!");
              
-             Parser parser = new Parser(".");
+             Parser majorParser = new Parser("FW_VERSION_MAJOR  = 1;");
+             Parser minorParser = new Parser("FW_VERSION_MINOR  = 64;");
+             Parser bugfixParser = new Parser("FW_VERSION_BUGFIX = 4;");
+             Parser buildParser = new Parser("FW_VERSION_BUILD  = 3;");
              
-             int major = parser.parseStatement("FW_VERSION_MAJOR  = 1;");
+             int major = majorParser.parseStatement("FW_VERSION_MAJOR  = 1;");
+             int minor = minorParser.parseStatement("FW_VERSION_MAJOR  = 64;");
+             int bugfix = bugfixParser.parseStatement("FW_VERSION_MAJOR  = 4;");
+             int build = buildParser.parseStatement("FW_VERSION_MAJOR  = 3;");
              
              System.out.println("major = " + major);
+             System.out.println("minor = " + minor);
+             System.out.println("bugfix = " + bugfix);
+             System.out.println("build = " + build);
        }
 }
