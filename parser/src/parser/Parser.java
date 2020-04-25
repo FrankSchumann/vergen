8ecb5d2acd327bd89d573c8efb4f8b3c1da2e5ef
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-25 13:11:55 +0200

    parse PacConfigVersion.hpp

--- parser/src/parser/Parser.java
+++ parser/src/parser/Parser.java
@@ -13,19 +13,35 @@ public class Parser {
 	public int parseStatement(String statement) {
 		System.out.println("run");
 		
-		String line = statement.replaceAll(regEx, "");
+		String line = getRegExMatch(statement);
 		
 		System.out.println("line = " + line);
 		
-		int number = detectNumber(statement);
+		int number = detectNumber(line);
 
 		System.out.println("number = " + number);
 
 		return number;
 	}
+	
+	private String getRegExMatch(String statement)
+	{
+		Pattern pattern = Pattern.compile(regEx);
+		Matcher matcher = pattern.matcher(statement);
+		
+		String result = "";
+		if (matcher.find()) {
+		    result = matcher.group();
+		    System.out.println("result = " + result);
+		}
+		
+		return result;
+	}
 
 	private int detectNumber(String input) {
 		String number = input.replaceAll("[^0-9?]", "");
+		
+		System.out.println("number = " + number);
 
 		return (Integer.parseInt(number));
 	}
