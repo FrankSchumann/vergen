c754e850a3497ab06f23ab965c207573dff341b0
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-25 11:34:13 +0200

    add method detectNumber

--- parser/src/parser/Parser.java
+++ parser/src/parser/Parser.java
@@ -5,41 +5,28 @@ import java.util.regex.*;
 
 public class Parser {
 	String regEx = "";
-
-	int major = 0;
-	int minor = 0;
-	int bugfix = 0;
-	int build = 0;
-
-	public int getMajor() {
-		return major;
-	}
-
-	public int getMinor() {
-		return minor;
-	}
-
-	public int getBugfix() {
-		return bugfix;
-	}
-
-	public int getBuild() {
-		return build;
-	}
-
-
+	
 	public Parser(String regEx) {
 		this.regEx = regEx;
 	}
 
 	public int parseStatement(String statement) {
 		System.out.println("run");
+
+		//String number = statement.replaceAll("[^0-9?]", "");
 		
-		String number = statement.replaceAll("[^0-9?]", "");
+		int number = detectNumber(statement);
 
 		System.out.println("number = " + number);
-		
-		return(Integer.parseInt(number));
+
+		//return (Integer.parseInt(number));
+		return number;
+	}
+
+	private int detectNumber(String s) {
+		String number = s.replaceAll("[^0-9?]", "");
+
+		return (Integer.parseInt(number));
 	}
 
 }
