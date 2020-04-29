b2a6082bf7455914599bfe2e18834e167c921c32
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-29 13:51:31 +0200

    extract handling of version into class Version

--- gradle/gradle_prototype/src/main/java/generator/Generator.java
+++ gradle/gradle_prototype/src/main/java/generator/Generator.java
@@ -5,13 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
 import java.util.Map;
+import com.hubspot.jinjava.Jinjava;
 
 public class Generator {
 	Path templatePath;
-
-	Map<String, Object> context;
+	Version version;
 
 	public void setTemplateFile(String fileName) {
 		this.templatePath = Paths.get(fileName);
@@ -24,9 +23,12 @@ public class Generator {
 	public void generate(String outputFileName) {
 		try {
 			String template = new String(Files.readAllBytes(templatePath));
-
-//		String renderedTemplate = jinjava2.render(template, context);
-			String renderedTemplate = "";
+			
+			Map<String, Object> context = version.getContext();
+			
+			Jinjava jinjava2 = new Jinjava();
+			
+			String renderedTemplate = jinjava2.render(template, context);
 
 			Path outputFileNamePath = Paths.get(outputFileName);
 
@@ -38,14 +40,9 @@ public class Generator {
 			e.printStackTrace();
 		}
 	}
-
-	public void setVersion(String major, String minor, String bugfix, String build) {
-		context = new HashMap<>();
-
-		context.put("major", major);
-		context.put("minor", minor);
-		context.put("bugfix", bugfix);
-		context.put("build", build);
+	
+	public void setVersion(Version version) {
+		this.version = version;
 	}
 
 }
---
+++ gradle/gradle_prototype/src/main/java/generator/VersionException.java
@@ -0,0 +1,16 @@
+package generator;
+
+public class VersionException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6877869655700066145L;
+
+	public VersionException() {
+	}
+
+	public VersionException(String s) {
+		super(s);
+	}
+}
