d2446848ec907c62c82d3fd65f552d23113130a3
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-29 13:51:04 +0200

    fix warning

--- gradle/gradle_prototype/src/main/java/parser/ParserException.java
+++ gradle/gradle_prototype/src/main/java/parser/ParserException.java
@@ -2,6 +2,11 @@ package parser;
 
 public class ParserException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1254778942251857496L;
+
 	public ParserException() {
 	}
 
