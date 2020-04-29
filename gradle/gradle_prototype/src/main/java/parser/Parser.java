433384c1384cfe0ce293b0230c52357268a981b5
Frank Schumann <frank.schumann.76@gmail.com>
2020-04-29 11:58:43 +0200

    add package parser

---
+++ gradle/gradle_prototype/src/main/java/parser/Parser.java
@@ -0,0 +1,41 @@
+package parser;
+
+import java.util.regex.*;
+
+public class Parser {
+	String regEx = "";
+
+	public Parser(String regEx) {
+		this.regEx = regEx;
+	}
+
+	public int parseStatement(String statement) {
+
+		String line = getRegExMatch(statement);
+
+		int number = detectNumber(line);
+
+		return number;
+	}
+
+	private String getRegExMatch(String statement) throws ParserException {
+		Pattern pattern = Pattern.compile(regEx);
+		Matcher matcher = pattern.matcher(statement);
+
+		String result = "";
+		if (matcher.find()) {
+			result = matcher.group();
+		} else {
+			throw new ParserException("regEx doesn't match");
+		}
+
+		return result;
+	}
+
+	private int detectNumber(String input) {
+		String number = input.replaceAll("[^0-9?]", "");
+
+		return (Integer.parseInt(number));
+	}
+
+}
