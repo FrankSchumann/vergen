8ff6aa547f6d26720b1db1c0fad73649b20fc1df
Frank Schumann <frank@udos-imac.speedport_w_724v_09011603_06_006>
2020-05-13 21:31:02 +0200

    remove plugin examples, the examples are added to playground repository

--- plugin/plugin_prototype3/src/org/vergen/views/commands/Command.java
+++
@@ -1,7 +0,0 @@
-package org.vergen.views.commands;
-
-public interface Command {
-	
-	public void execute();
-
-}
--- plugin/plugin_prototype3/src/org/vergen/views/commands/Invoker.java
+++
@@ -1,9 +0,0 @@
-package org.vergen.views.commands;
-
-public class Invoker {
-
-	public void executeCommand(Command command) {
-		command.execute();
-	}
-
-}
