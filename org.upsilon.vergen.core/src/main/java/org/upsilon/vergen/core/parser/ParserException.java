38a6583807484417c3e8727be6b4fed2d942f02c
Frank Schumann <frank@udos-imac.speedport_w_724v_09011603_06_006>
2020-05-17 11:32:53 +0200

    add parser, compiling with maven

---
+++ org.upsilon.vergen.core/src/main/java/org/upsilon/vergen/core/parser/ParserException.java
@@ -0,0 +1,16 @@
+package org.upsilon.vergen.core.parser;
+
+public class ParserException extends RuntimeException {
+
+
+	public static final String REGEX_NO_MATCH = "RegEx doesn't match";
+	
+	private static final long serialVersionUID = 1254778942251857496L;
+
+	public ParserException() {
+	}
+
+	public ParserException(String s) {
+		super(s);
+	}
+}
