5e5c7f31041d5a608c2442c5c82f7b46b9108ba7
Frank Schumann <frank@udos-imac.speedport_w_724v_09011603_06_006>
2020-05-17 11:42:28 +0200

    add Generator, compiling with maven

---
+++ org.upsilon.vergen.core/src/main/java/org/upsilon/vergen/core/generator/Generator.java
@@ -0,0 +1,42 @@
+package org.upsilon.vergen.core.generator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Map;
+import com.hubspot.jinjava.Jinjava;
+
+
+public class Generator {
+	private Path templatePath;
+	private Version version;
+
+	public Generator(String templateFileName, Version version) {
+		this.templatePath = Paths.get(templateFileName);
+		this.version = version;
+	}
+
+	public void generate(String outputFileName) {
+		try {
+			String template = new String(Files.readAllBytes(templatePath));
+			
+			Map<String, Object> context = version.getContext();
+			
+			Jinjava jinjava2 = new Jinjava();
+			
+			String renderedTemplate = jinjava2.render(template, context);
+
+			Path outputFileNamePath = Paths.get(outputFileName);
+
+			Files.write(outputFileNamePath, renderedTemplate.getBytes(), StandardOpenOption.CREATE,
+					StandardOpenOption.TRUNCATE_EXISTING);
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
