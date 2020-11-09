e84cf944ab1ef0ea0499776624d1f8a46a07c836
Frank Schumann <frank.schumann@se.com>
2020-11-09 23:17:09 +0100

    remove not used braces

--- src/org/vergen/preferences/ParserPage.java
+++ src/org/vergen/preferences/ParserPage.java
@@ -37,52 +37,44 @@ public class ParserPage extends FieldEditorPreferencePage implements IWorkbenchP
 	 * knows how to save and restore itself.
 	 */
 	public void createFieldEditors() {
-		{
-				editSettingsBooleanFieldEditor = new BooleanFieldEditor(Constants.PREF_EDIT_SETTINGS, Messages.editSettingsLabelText,
-					BooleanFieldEditor.DEFAULT, getFieldEditorParent()) {
+		editSettingsBooleanFieldEditor = new BooleanFieldEditor(Constants.PREF_EDIT_SETTINGS,
+				Messages.editSettingsLabelText, BooleanFieldEditor.DEFAULT, getFieldEditorParent()) {
 
-				@Override
-				protected void valueChanged(boolean oldValue, boolean newValue) {
-					super.valueChanged(oldValue, newValue);
-					setEnabledStringFieldEditors(newValue);
-				}
-			};
+			@Override
+			protected void valueChanged(boolean oldValue, boolean newValue) {
+				super.valueChanged(oldValue, newValue);
+				setEnabledStringFieldEditors(newValue);
+			}
+		};
+		addField(editSettingsBooleanFieldEditor);
 
-			addField(editSettingsBooleanFieldEditor);
-		}
-		{
-			majorStringFieldEditor = new StringFieldEditor(Constants.PREF_MAJOR_REGEX, Messages.majorLabelText, -1,
-					StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
-			majorStringFieldEditor.setEmptyStringAllowed(false);
-			majorStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
-			addField(majorStringFieldEditor);
-		}
-		{
-			minorStringFieldEditor = new StringFieldEditor(Constants.PREF_MINOR_REGEX, Messages.minorLabelText, -1,
-					StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
-			minorStringFieldEditor.setEmptyStringAllowed(false);
-			minorStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
-			addField(minorStringFieldEditor);
-		}
-		{
-			bugfixStringFieldEditor = new StringFieldEditor(Constants.PREF_BUGFIX_REGEX, Messages.bugfixLabelText, -1,
-					StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
-			bugfixStringFieldEditor.setEmptyStringAllowed(false);
-			bugfixStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
-			addField(bugfixStringFieldEditor);
-		}
-		{
-			buildStringFieldEditor = new StringFieldEditor(Constants.PREF_BUILD_REGEX, Messages.buildLabelText, -1,
-					StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
-			buildStringFieldEditor.setEmptyStringAllowed(false);
-			buildStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
-			addField(buildStringFieldEditor);
-		}
-		{
-			org.vergen.preferences.Preferences preferences = new Preferences();
-			boolean editSettingsPreference = preferences.getEditSettingsPreference();
-			setEnabledStringFieldEditors(editSettingsPreference);
-		}
+		majorStringFieldEditor = new StringFieldEditor(Constants.PREF_MAJOR_REGEX, Messages.majorLabelText, -1,
+				StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
+		majorStringFieldEditor.setEmptyStringAllowed(false);
+		majorStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
+		addField(majorStringFieldEditor);
+
+		minorStringFieldEditor = new StringFieldEditor(Constants.PREF_MINOR_REGEX, Messages.minorLabelText, -1,
+				StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
+		minorStringFieldEditor.setEmptyStringAllowed(false);
+		minorStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
+		addField(minorStringFieldEditor);
+
+		bugfixStringFieldEditor = new StringFieldEditor(Constants.PREF_BUGFIX_REGEX, Messages.bugfixLabelText, -1,
+				StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
+		bugfixStringFieldEditor.setEmptyStringAllowed(false);
+		bugfixStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
+		addField(bugfixStringFieldEditor);
+
+		buildStringFieldEditor = new StringFieldEditor(Constants.PREF_BUILD_REGEX, Messages.buildLabelText, -1,
+				StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
+		buildStringFieldEditor.setEmptyStringAllowed(false);
+		buildStringFieldEditor.setEnabled(false, this.getFieldEditorParent());
+		addField(buildStringFieldEditor);
+
+		org.vergen.preferences.Preferences preferences = new Preferences();
+		boolean editSettingsPreference = preferences.getEditSettingsPreference();
+		setEnabledStringFieldEditors(editSettingsPreference);
 	}
 	
 	public void init(IWorkbench workbench) {
