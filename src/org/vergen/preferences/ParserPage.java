1b0a96d1a4dbf96d0cd9fa47cb6ef0b986ca2d4a
Frank Schumann <frank.schumann@se.com>
2020-11-09 23:17:37 +0100

    remove not used comment

--- src/org/vergen/preferences/ParserPage.java
+++ src/org/vergen/preferences/ParserPage.java
@@ -5,17 +5,6 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.vergen.Activator;
 import org.eclipse.ui.IWorkbench;
 
-/**
- * This class represents a preference page that is contributed to the
- * Preferences dialog. By subclassing <samp>FieldEditorPreferencePage</samp>, we
- * can use the field support built into JFace that allows us to create a page
- * that is small and knows how to save, restore and apply itself.
- * <p>
- * This page is used to modify preferences only. They are stored in the
- * preference store that belongs to the main plug-in class. That way,
- * preferences can be accessed directly via the preference store.
- */
-
 public class ParserPage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	private BooleanFieldEditor editSettingsBooleanFieldEditor;
